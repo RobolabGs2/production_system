e0edbade54566977c30a7b046d6b09eccbbf8f9b
RobolabGs2 <slavikelk@yandex.ru>
2020-11-05 23:22:28 +0300

    Add parsing

--- src/http_helpers.ts
+++ src/http_helpers.ts
@@ -8,6 +8,16 @@ export function fetchJSON(url: RequestInfo): Promise<any> {
         }).then(response => response.json())
 }
 
+export function fetchTxt(url: RequestInfo): Promise<string> {
+    return fetch(url,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        }).then(response => response.text())
+}
+
 export function fetchImage(name: string): Promise<ImageBitmapSource>
 {
     return new Promise(function(resolve, reject)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+import { fetchTxt } from "./http_helpers";
 import "./style.scss"
 
 console.log("Hello world");
 document.querySelector("header")!.innerText = "Hello world!";
+
+Promise.all(
+    ["Substances", "Chemistry"].
+        map(x => `/resources/${x}.txt`).
+        map(fetchTxt)
+).then(([substances, reactions]) => {
+    const names = new Map<string, string>();
+    const notFound = new Set<string>();
+    substances.split('\n').filter(x=>x.trim() != "").map(s => s.split(" -> ")).forEach(pair => names.set(pair[0].trim(), pair[1].trim()));
+    document.querySelector("section")!.innerText = reactions.replace(/[A-Za-zС0-9()]+/g, (substr)=>{
+        const name = names.get(substr);
+        if (!name) notFound.add(substr);
+        return name ? name : substr;
+        }
+    );
+    console.log(Array.from(notFound.keys()).join("\n"));
+});
